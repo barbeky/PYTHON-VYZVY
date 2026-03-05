72d48de93d508d988a9acdf14ee5188b2e62d46c
barbeky <bobakovarebeka@gmail.com>
2026-03-05 11:46:52 +0100

    Update python_16_narocna_rebricek_top3.py

--- python_16_narocna_rebricek_top3.py
+++ python_16_narocna_rebricek_top3.py
@@ -1,4 +1,5 @@
-# Náročná úroveň – Rebríček TOP 3
+# Náročná úroveň – Rebríček TOP 3 
+# Tabuľka hráčov je zoznam zoznamov: [meno, body]
 
 hraci = []
 
@@ -7,7 +8,27 @@ for i in range(3):
     body = int(input("Zadaj pocet bodov: "))
     hraci.append([meno, body])
 
-hraci.sort(key=lambda x: x[1], reverse=True)
+# 1. miesto
+prvy = hraci[0]
+for hrac in hraci:
+    if hrac[1] > prvy[1]:
+        prvy = hrac
 
-for i in range(min(3, len(hraci))):
-    print(i+1, ".", hraci[i][0], "-", hraci[i][1], "bodov")
+# 2. miesto
+druhy = None
+for hrac in hraci:
+    if hrac != prvy:
+        if druhy is None or hrac[1] > druhy[1]:
+            druhy = hrac
+
+# 3. miesto
+treti = None
+for hrac in hraci:
+    if hrac != prvy and hrac != druhy:
+        if treti is None or hrac[1] > treti[1]:
+            treti = hrac
+
+print("Rebricek TOP 3:")
+print("1.", prvy[0], "-", prvy[1], "bodov")
+print("2.", druhy[0], "-", druhy[1], "bodov")
+print("3.", treti[0], "-", treti[1], "bodov")
