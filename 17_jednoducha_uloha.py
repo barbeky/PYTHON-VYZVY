1e3ff4d90627bf063217c9a0eba61959af355813
barbeky <bobakovarebeka@gmail.com>
2026-03-10 22:26:53 +0100

    Add files via upload

---
+++ 17_jednoducha_uloha.py
@@ -0,0 +1,8 @@
+# 17. tyzden: Jednoducha uloha - Prve a posledne znaky
+text = input("Zadaj text: ")
+
+prve_tri = text[:3]
+posledne_tri = text[-3:]
+
+print(f"Prve 3 znaky: {prve_tri}")
+print(f"Posledne 3 znaky: {posledne_tri}")
---
+++ 17_stredna_uloha.py
@@ -0,0 +1,8 @@
+# 17. tyzden: Stredna uloha - Podretazec podla indexov
+text = input("Zadaj text: ")
+od = int(input("Zadaj od: "))
+do = int(input("Zadaj do: "))
+
+podretazec = text[od:do]
+
+print(f"Podretazec: {podretazec}")
