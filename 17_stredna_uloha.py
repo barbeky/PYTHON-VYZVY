b30a587bdf458bd12124f284ce8aa9a0eb5dfce8
barbeky <bobakovarebeka@gmail.com>
2026-03-13 09:32:33 +0100

    Delete 17_stredna_uloha.py

--- 17_stredna_uloha.py
+++
@@ -1,8 +0,0 @@
-# 17. tyzden: Stredna uloha - Podretazec podla indexov
-text = input("Zadaj text: ")
-od = int(input("Zadaj od: "))
-do = int(input("Zadaj do: "))
-
-podretazec = text[od:do]
-
-print(f"Podretazec: {podretazec}")
