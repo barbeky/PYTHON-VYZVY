bbbe6098213cad194b08e0b9bde74fab616f6739
barbeky <bobakovarebeka@gmail.com>
2026-03-13 09:03:25 +0100

    Delete 17_jednoducha_uloha.py

--- 17_jednoducha_uloha.py
+++
@@ -1,9 +0,0 @@
-# 17. tyzden: Jednoducha uloha - Prve a posledne znaky
-text = input("Zadaj text: ")
-
-prve_tri = text[:3]
-posledne_tri = text[-3:]
-
-print("Prve 3 znaky: ", prve_tri)
-print("Posledne 3 znaky: ", posledne_tri)
-
